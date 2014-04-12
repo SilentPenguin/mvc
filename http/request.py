72ff15b3af394db27b307091cdd74558545b8e6a
Silent Penguin <seppy@Silent-Penguins-iMac-2.local>
2014-04-12 19:12:15 +0100

    Pulled Url parsing out of request class. Moved QueryString to url module

--- http/request.py
+++ http/request.py
@@ -1,10 +1,8 @@
 from mvc.http.method import Method
-    from mvc.http.query_string import QueryString
+from mvc.url.url import Url
 from urllib import quote
 
 class Request ():
-    
-    DEFAULT_PORTS = {'http': 80, 'https': 443}
     
     environment = None
     wsgi = {}
@@ -54,24 +52,11 @@ class Request ():
         if 'server_port' in self:
             self.server_port = int(self.server_port)
 
-        if 'query_string' in self:
-            self.query_string = QueryString(self.query_string)
-
     #adds in some extra properties to save on some faff
     def _extras(self, environment):
-        self.path = quote(self.script_name + self.path_info, '')
-        self.host = self.http_host if 'http_host' in self else self.server_name
-        self.secured = self.wsgi.url_scheme in SECURE_PROTOCOLS
-        self.address = self._construct_address();
-    
-    def _construct_address(self):
-        url = self.wsgi.url_scheme '://' + self.host
-        if self.url_scheme in DEFAULT_PORTS \
-                and self.server_port != DEFAULT_PORTS[self.url_scheme]:
-            url += ':' + self.server_port
-        url += self.path
-        if 'query_string' in self:
-            url += '?' + self.query_string
+        path = quote(self.script_name + self.path_info, '')
+        host = self.http_host if 'http_host' in self else self.server_name
+        self.url = Url(host, path, query_string, self.url_scheme, self.server_port)    
 
     def __getitem__(self, index):
         return getattr(self,index)
