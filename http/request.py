8869ebfc5d924677d1d3bb26e35f04373129da29
Silent Penguin <seppy@Silent-Penguins-iMac-2.local>
2014-04-12 17:18:29 +0100

    Some helpers to make Request easier to work with

--- http/request.py
+++ http/request.py
@@ -1,25 +1,53 @@
 from mvc.http.method import Method
+from urllib import quote
 
 class Request ():
     
-    request_method = Method("unrecognised")
-    script_name = ""
-    path_info = ""
-    query_string = ""
-    content_type = ""
+    environment = None
+    request_method = Method('unrecognised')
+    script_name = ''
+    path_info = ''
+    query_string = ''
+    content_type = ''
     content_length = 0
-    server_name = ""
-    server_port = ""
-    server_protocol = ""
+    server_name = ''
+    server_port = ''
+    server_protocol = ''
+    
+    application_path = ''
 
     def __init__(self, environment):
     
-        #copy our environment across to the request attributes
+        self.environment = environment
+    
+        self._clone(environment)
+        self._pleasentaries(environment)
+        self._extras(environment)
+
+    #copy our environment across to the request as properties
+    def _clone(self, environment)
         for name, value in environment.items():
             setattr(self, name.lower(), value);
 
-        #make some of the present environment values more palletable
-        if "REQUEST_METHOD" in environment:
+    #make some of the present environment values more palletable
+    def _pleasentaries(self, environment):
+        if 'request_method' in self:
             self.request_method = Method(self.request_method)
-        if "CONTENT_LENGTH" in environment:
+        
+        if 'content_length' in self:
             self.content_length = int(self.content_length)
+
+        if 'server_port' in self:
+            self.server_port = int(self.server_port)
+
+    #adds in some extra properties to save on some faff
+    def _extras(self, environment):
+        self.application_path = quote(self.script_name, '') + quote(self.path_info, '')
+        self.host = self.http_host if 'http_host' in self else self.server_name
+
+    def __getitem__(self, index):
+        return getattr(self,index)
+
+    def __contains__(self, index):
+        return index in self.environment or hasattr(self, index)
+        
